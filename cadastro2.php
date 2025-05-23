19a8e8c3020a02d35968e12606a68d385c4db994
Enzo Andrade <82385947+treasurestar@users.noreply.github.com>
2025-05-23 10:25:18 -0300

    Add files via upload

---
+++ cadastro2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Confirmação Cadastro</h1>
+    <h3>Dados Reecebidos</h3>
+    <p>Nome: <?php echo $_POST["nome"]; ?></p>
+    <p>Sobrenome: <?php echo $_POST["sobrenome"]; ?></p>
+</body>
+</html>
